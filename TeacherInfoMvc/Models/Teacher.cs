f2bab0d841610bcde652ad5336fb4963549daf12
18sancheet <sancheetwalekar@gmail.com>
2022-07-17 22:12:58 +0530

    first commit

---
+++ TeacherInfoMvc/Models/Teacher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeacherInfoMvc.Models
+{
+    public class Teacher
+    {
+        public virtual int TeacherId
+        { get; set; }
+
+        public virtual String TeacherName
+        { get; set; }
+
+        public virtual String TeacherQual
+        { get; set; }
+
+        public virtual int YearsOfExp
+        { get; set; }
+    }
+}
